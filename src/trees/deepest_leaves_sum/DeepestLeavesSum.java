afb2e5580d7d3fe071851411465e3ce138024332
Ilya Titov <bkmz1941f@gmail.com>
2024-04-05 10:24:23 +0200

    feat: DeepestLeavesSum was updated

--- src/trees/deepest_leaves_sum/DeepestLeavesSum.java
+++ src/trees/deepest_leaves_sum/DeepestLeavesSum.java
@@ -1,5 +1,6 @@
 package trees.deepest_leaves_sum;
 
+import trees.helpers.Pair;
 import trees.helpers.SimpleTreeNode;
 import trees.helpers.Trees;
 
@@ -11,11 +12,40 @@ import java.util.ArrayDeque;
 public class DeepestLeavesSum {
     public static void main(String[] args) {
         SimpleTreeNode root = Trees.generateTree(new Integer[]{1,2,3,4,5,null,6,7,null,null,null,null,8});
-        int answer = deepestLeavesSum(root);
-        System.out.println(answer);
+        int answer1 = deepestLeavesSumBFS(root);
+        int answer2 = deepestLeavesSumDFS(root);
+        System.out.println(answer1); // 15
+        System.out.println(answer2); // 15
     }
 
-    public static int deepestLeavesSum(SimpleTreeNode node) {
+    public static int deepestLeavesSumDFS(SimpleTreeNode node) {
+        if (node == null) return 0;
+
+        int answer = 0, maxDepth = 0, currDepth = 0;
+        ArrayDeque<Pair> deque = new ArrayDeque<>();
+        deque.add(new Pair(node, 0));
+
+        while (!deque.isEmpty()) {
+            Pair pair = deque.remove();
+            SimpleTreeNode pairNode = pair.node;
+            int pairDepth = pair.depth;
+            if (pairNode.left == null && pairNode.right == null) {
+                if (maxDepth < pairDepth) {
+                    maxDepth = pairDepth;
+                    answer = pairNode.value;
+                } else if (maxDepth == pairDepth) {
+                    answer += pairNode.value;
+                }
+            } else {
+                if (pairNode.left != null) deque.add(new Pair(pairNode.left, pairDepth + 1));
+                if (pairNode.right != null) deque.add(new Pair(pairNode.right, pairDepth + 1));
+            }
+        }
+
+        return answer;
+    }
+
+    public static int deepestLeavesSumBFS(SimpleTreeNode node) {
         if (node == null) return 0;
 
         ArrayDeque<SimpleTreeNode> currQueue = new ArrayDeque<>();
